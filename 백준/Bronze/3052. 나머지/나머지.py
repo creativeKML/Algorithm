ef75f8c29b8064d77f7bec2ae41959290139f15e
creativeKML <martinychimmy@gmail.com>
2026-03-09 17:47:50 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,7 +1,7 @@
-remainders = set()
+n = []
 
-for _ in range(10):
+for i in range(10):
     num = int(input())
-    remainders.add(num % 42)
-
-print(len(remainders))
+    n.append(num % 42)
+    
+print(len(set(n)))
